3bd9b3ee109c034fd8c311ac765ef2d89b18393b
CROBOT975 <74949016+CROBOT975@users.noreply.github.com>
2022-01-21 21:15:54 +0800

    Add files via upload

---
+++ gym_foodhunting/__init__.py
@@ -0,0 +1,85 @@
+from gym.envs.registration import register
+from gym_foodhunting.foodhunting.gym_foodhunting import HSR, HSRSimple
+from gym_foodhunting.foodhunting.maze import HSR, HSRSimple
+from gym_foodhunting.foodhunting.grasping import HSR, HSRSimple
+# FoodHunting HSR
+register(
+    id='FoodHuntingHSR-v0',
+    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
+    max_episode_steps=50,
+    kwargs={'render': False,
+            'robot_model': HSRSimple,
+            'max_steps': 50,
+            'num_foods': 1,
+            'num_fakes': 1,
+            'object_size': 0.5,
+            'object_radius_scale': 1.0,
+            'object_radius_offset': 1.0,
+            'object_angle_scale': 1.0}
+)
+
+register(
+    id='FoodHuntingHSRGUI-v0',
+    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
+    max_episode_steps=50,
+    kwargs={'render': True, 'robot_model': HSRSimple, 'max_steps': 50, 'num_foods': 1, 'num_fakes': 1, 'object_size': 0.5, 'object_radius_scale': 1.0, 'object_radius_offset': 1.0, 'object_angle_scale': 1.0}
+)
+
+register(
+    id='MazeNavigatingHSR-v0',
+    entry_point='gym_foodhunting.foodhunting:MazeEnv',
+    max_episode_steps=50,
+    kwargs={'render': False,
+            'robot_model': HSRSimple,
+            'max_steps': 50,
+            'num_foods': 1,
+            'num_fakes': 0,
+            'object_size': 0.5,
+            'object_radius_scale': 1.0,
+            'object_radius_offset': 1.0,
+            'object_angle_scale': 1.0}
+)
+
+register(
+    id='MazeNavigatingHSRGUI-v0',
+    entry_point='gym_foodhunting.foodhunting:MazeEnv',
+    max_episode_steps=50,
+    kwargs={'render': True, 'robot_model': HSRSimple, 'max_steps': 50, 'num_foods': 1, 'num_fakes': 0, 'object_size': 0.5, 'object_radius_scale': 1.0, 'object_radius_offset': 1.0, 'object_angle_scale': 1.0}
+)
+
+register(
+    id='GraspingHSR-v0',
+    entry_point='gym_foodhunting.foodhunting:GraspingEnv',
+    max_episode_steps=50,
+    kwargs={'render': False,
+            'robot_model': HSRSimple,
+            'max_steps': 50,
+            'num_foods': 1,
+            'num_fakes': 0,
+            'object_size': 0.1,
+            'object_radius_scale': 0.1,
+            'object_radius_offset': 0.5,
+            'object_angle_scale': 0.1}
+)
+
+register(
+    id='GraspingHSRGUI-v0',
+    entry_point='gym_foodhunting.foodhunting:GraspingEnv',
+    max_episode_steps=50,
+    kwargs={'render': True, 'robot_model': HSRSimple, 'max_steps': 50, 'num_foods': 1, 'num_fakes': 0, 'object_size': 0.1, 'object_radius_scale': 0.1, 'object_radius_offset': 0.5, 'object_angle_scale': 0.1}
+)
+
+
+register(
+    id='FoodHuntingHSRTestGUI-v0',
+    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
+    max_episode_steps=10000,
+    kwargs={'render': True, 'robot_model': HSR, 'max_steps': 10000, 'num_foods': 3, 'num_fakes': 3, 'object_size': 0.5, 'object_radius_scale': 1.0, 'object_radius_offset': 1.0, 'object_angle_scale': 1.0}
+)
+
+register(
+    id='FoodHuntingHSRTestGUI-v1',
+    entry_point='gym_foodhunting.foodhunting:FoodHuntingEnv',
+    max_episode_steps=10000,
+    kwargs={'render': True, 'robot_model': HSRSimple, 'max_steps': 10000, 'num_foods': 1, 'num_fakes': 0, 'object_size': 0.5, 'object_radius_scale': 0.0, 'object_radius_offset': 1.5, 'object_angle_scale': 0.25}
+)
---
+++ gym_foodhunting/foodhunting/__init__.py
@@ -0,0 +1,3 @@
+from gym_foodhunting.foodhunting.gym_foodhunting import FoodHuntingEnv
+from gym_foodhunting.foodhunting.maze import MazeEnv
+from gym_foodhunting.foodhunting.grasping import GraspingEnv
