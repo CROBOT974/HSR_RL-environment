6b29cf7632e4ab41f6872afa8f21d379f16242a0
CROBOT975 <74949016+CROBOT975@users.noreply.github.com>
2022-01-28 15:21:11 +0800

    Update gym_foodhunting.py

--- gym_foodhunting/foodhunting/gym_foodhunting.py
+++ gym_foodhunting/foodhunting/gym_foodhunting.py
@@ -221,7 +221,7 @@ class Robot:
         self.printJointInfoArray(range(p.getNumJoints(self.robotId)))
 
 
-class HSR(Robot):
+class HSR_FH(Robot):
     URDF_PATH = 'hsrb4s.urdf'
 
     # viewMatrix settings
@@ -235,7 +235,7 @@ class HSR(Robot):
     def __init__(self, urdfPath=URDF_PATH, position=[0.0, 0.0, 0.05], orientation=[0.0, 0.0, 0.0, 1.0]):
         """Make a HSR robot model.
         """
-        super(HSR, self).__init__(urdfPath, position, orientation)
+        super(HSR_FH, self).__init__(urdfPath, position, orientation)
 
     # override methods
     @classmethod
@@ -352,7 +352,7 @@ class HSR(Robot):
         return self.getJointPosition(30),  self.getJointPosition(32)
 
 
-class HSRSimple(HSR):
+class HSRSimple_FH(HSR_FH):
     @classmethod
     def getActionSpace(cls):
         """Return action_space for gym Env class.
@@ -380,7 +380,7 @@ class FoodHuntingEnv(gym.Env):
     GRAVITY = -10.0
     BULLET_STEPS = 120 # p.setTimeStep(1.0 / 240.0), so 1 gym step == 0.5 sec.
 
-    def __init__(self, render=False, robot_model=HSRSimple, max_steps=500, num_foods=3, num_fakes=0, object_size=1.0, object_radius_scale=1.0, object_radius_offset=1.0, object_angle_scale=1.0):
+    def __init__(self, render=False, robot_model=HSRSimple_FH, max_steps=500, num_foods=3, num_fakes=0, object_size=1.0, object_radius_scale=1.0, object_radius_offset=1.0, object_angle_scale=1.0):
         """Initialize environment.
         """
         ### gym variables
