2ffc497300e9720b1bdb25e10d712b9faae7e5ba
CROBOT975 <74949016+CROBOT975@users.noreply.github.com>
2022-02-09 15:53:12 +0800

    setup

---
+++ PyHSR/HSR_RL_env/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='HSR_RL_env',
+      version='0.1.0',
+      install_requires=['numpy', 'gym', 'opencv-python', 'pybullet'] # and stable-baselines3 for running RL
+)
