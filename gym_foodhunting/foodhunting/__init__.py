13668c86569984c9e8a244240fda4093d482ec6f
CROBOT975 <74949016+CROBOT975@users.noreply.github.com>
2022-02-09 15:50:53 +0800

    Delete __init__.py

--- gym_foodhunting/foodhunting/__init__.py
+++
@@ -1,3 +0,0 @@
-from gym_foodhunting.foodhunting.gym_foodhunting import FoodHuntingEnv
-from gym_foodhunting.foodhunting.maze import MazeEnv
-from gym_foodhunting.foodhunting.grasping import GraspingEnv
